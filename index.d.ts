38eb65c852a18be4ffd751a484798836b6d1fe82
LuongLuyen <luongvanluyen2003@gmail.com>
2024-03-21 17:47:34 +0700

    Test config  Lib

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare function Sum(a: number): number;
+declare const Name: string
+
+export {Sum, Name} ;
---
+++ index.js
@@ -0,0 +1,5 @@
+const Sum =(a)=>{
+    return a+1
+}
+const Name = "hello"
+export {Sum,Name}
