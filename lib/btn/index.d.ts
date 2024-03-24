ff88beb8946dbbc332732f003172982b2ca71498
LuongLuyen <luongvanluyen2003@gmail.com>
2024-03-24 23:53:37 +0700

    Button

--- lib/btn/index.d.ts
+++ lib/btn/index.d.ts
@@ -1,2 +1,19 @@
-declare const ButtonW: string;
-export {ButtonW};
+declare function ButtonW(name: string): string;
+declare function Button1(name: string): string;
+declare function Button2(name: string): string;
+declare function Button3(name: string): string;
+declare function Button4(name: string): string;
+declare function Button5(name: string): string;
+declare function Button6(name: string): string;
+declare function Button7(name: string): string;
+declare function Button8(name: string): string;
+declare function Button9(name: string): string;
+declare function Button10(name: string): string;
+declare function Button11(name: string): string;
+declare function Button12(name: string): string;
+declare function Button13(name: string): string;
+declare function Button14(name: string): string;
+declare function Button15(name: string): string;
+declare function Button16(name: string): string;
+
+export{ButtonW,Button1,Button2,Button3,Button4,Button5,Button6,Button7,Button8,Button9,Button10,Button11,Button12,Button13,Button14,Button15,Button16};
