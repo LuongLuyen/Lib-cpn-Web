9401ccc05fedba5649b7ee63d770a7a87368378d
LuongLuyen <luongvanluyen2003@gmail.com>
2024-03-31 17:28:55 +0700

    cart pruduct

--- lib/card/index.d.ts
+++ lib/card/index.d.ts
@@ -1,3 +1,6 @@
 declare function Card1(api: string): string;
+declare function Card2(api: string): string;
+declare function Card3(dara: object,api:string): string;
+declare function Card4(api: string): string;
 
-export {Card1};
+export {Card1,Card2,Card3};
